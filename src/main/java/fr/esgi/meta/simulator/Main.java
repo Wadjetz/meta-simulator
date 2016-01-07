0446052dee858ccb7bafaf1635a6dceaf2c84330
Stitchou <la_muerte_97216@hotmail.com>
2016-01-07 12:52:37 +0100

    hide decorator test porting

--- src/main/java/fr/esgi/meta/simulator/Main.java
+++ src/main/java/fr/esgi/meta/simulator/Main.java
@@ -6,17 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Decorator dede = new Decorator();
-
         System.out.println("Hello World !");
 
+        /* Decorator c# porting
+
+        Decorator dede = new Decorator();
         Decorator.ComposantConcret c =  dede.new ComposantConcret();
         Decorator.DecorateurConcretA d1 = dede.new DecorateurConcretA();
         Decorator. DecorateurConcretB d2 = dede.new DecorateurConcretB();
 
         d1.SetComposant(c);
         d2.SetComposant(d1);
-        d2.Operation();
+        d2.Operation();*/
+
     }
 
 
