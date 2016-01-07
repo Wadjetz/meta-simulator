d8ce2f197c03fe2b032e9244ee48d3b3d048f3ff
Vuzi <vuzi@free.fr>
2016-01-07 11:44:32 +0100

    Simulation classes

---
+++ src/main/java/fr/esgi/meta/simulator/Board.java
@@ -0,0 +1,18 @@
+package fr.esgi.meta.simulator;
+
+/**
+ * Default board where units interact
+ *
+ * Created by vuzi on 07/01/2016.
+ */
+public abstract class Board {
+
+    Zone[][] zones;
+
+    public Board(int width, int height) {
+
+        zones = new Zone[width][height];
+        
+    }
+
+}
