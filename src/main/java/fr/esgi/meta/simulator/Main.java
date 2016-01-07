b5ec7e7cfb1af9acacefbfc1ca532290ff72f354
Stitchou <la_muerte_97216@hotmail.com>
2016-01-07 12:51:29 +0100

    add decorator basic class

--- src/main/java/fr/esgi/meta/simulator/Main.java
+++ src/main/java/fr/esgi/meta/simulator/Main.java
@@ -1,9 +1,23 @@
 package fr.esgi.meta.simulator;
 
+import fr.esgi.meta.pattern.Decorator;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Decorator dede = new Decorator();
+
         System.out.println("Hello World !");
+
+        Decorator.ComposantConcret c =  dede.new ComposantConcret();
+        Decorator.DecorateurConcretA d1 = dede.new DecorateurConcretA();
+        Decorator. DecorateurConcretB d2 = dede.new DecorateurConcretB();
+
+        d1.SetComposant(c);
+        d2.SetComposant(d1);
+        d2.Operation();
     }
+
 
 }
